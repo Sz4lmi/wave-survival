f59f82945c50f929c9545d5fde5645ee2ae57057
Sz4lmi <aron.szalmas@gmail.com>
2025-02-09 02:43:09 +0100

    one more upgrade
    rjsgnasldfkgjéadlkfgjésdlkfgj

--- player/arrow.gd
+++ player/arrow.gd
@@ -3,7 +3,9 @@ extends Area2D
 const SPEED = 1000
 var range = 1200
 var travelled_distance = 0
-var damage = 5
+var damage = 1.5
+var piercing = false
+var onfire = false
 
 
 func _physics_process(delta: float) -> void:
@@ -16,5 +18,8 @@ func _physics_process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("gethit"):
 		body.gethit(damage)
-	queue_free()
+		if body.has_method("setonfire"):
+			body.setonfire(3)
+		if piercing == false:
+			queue_free()
 	print(damage)
